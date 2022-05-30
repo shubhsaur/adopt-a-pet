72e867fb448f4a1112a944f36e7ae91383b98fe2
Shubham Saurabh <shubhamsaurabh@outlook.com>
2022-05-31 00:41:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Navigation from './components/navigation';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Route path="/">
+          <Navigation />
+        </Route>
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/pet-details-not-found">
+            <PetDetailsNotFound />
+          </Route>
+          <Route path="/:type/:id">
+            <PetDetailsPage />
+          </Route>
+          <Route path="/:type?">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
